dc4a65c0692d23335feef07cd3ef9e79aff3babd
alzhan-ismagulov <aismagulov@gmail.com>
2020-04-30 18:28:32 +0200

    02_03. Вывод брендов

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -8,6 +8,9 @@ class MainController extends AppController
 {
     public function indexAction()
     {
+        $brands = \R::find('brand', 'LIMIT 3');
+//        debug($brands);
         $this->setMeta('Главная страница', 'Описание...', 'Ключевики...');
+        $this->set(compact('brands')); //Передача в вид.
     }
 }
