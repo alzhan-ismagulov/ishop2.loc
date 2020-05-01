ba819026bb2a1529696a9f14585b1b254d567303
alzhan-ismagulov <aismagulov@gmail.com>
2020-05-01 11:41:41 +0200

    02_04. Популярные товары

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -9,8 +9,9 @@ class MainController extends AppController
     public function indexAction()
     {
         $brands = \R::find('brand', 'LIMIT 3');
+        $hits = \R::find('product', "hit = '1' AND status = '1' LIMIT 8");
 //        debug($brands);
         $this->setMeta('Главная страница', 'Описание...', 'Ключевики...');
-        $this->set(compact('brands')); //Передача в вид.
+        $this->set(compact('brands', 'hits')); //Передача в вид.
     }
 }
