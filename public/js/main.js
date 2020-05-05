4f15f07308bd4212854336b71f3eb9cbe25205a1
alzhan-ismagulov <aismagulov@gmail.com>
2020-05-06 01:18:04 +0200

    02_22. Корзина. Часть 5

--- public/js/main.js
+++ public/js/main.js
@@ -59,6 +59,19 @@ function getCart() {
         }
     });
 }
+
+function clearCart() {
+    $.ajax({
+        url: '/cart/clear',
+        type: 'GET',
+        success: function(res){
+            showCart(res);
+        },
+        error: function(){
+            alert('Ошибка! Попробуйте позже');
+        }
+    });
+}
 /*Cart*/
 
 $('#currency').change(function(){
