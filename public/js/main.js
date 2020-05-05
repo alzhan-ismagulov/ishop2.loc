aa237443280678b353718c6bd3f0330583a4c725
alzhan-ismagulov <aismagulov@gmail.com>
2020-05-05 17:49:07 +0200

    02_19. Корзина. Часть 2

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,26 @@
-/** cart */
-$('body').on('add-to-cart-link', function(e))
-{
-    e.preventDefault();
-    var $id = $(this).data('id'),
-        $qty = $('.quantity input').val() ? $('quantity input').val() : 1,
-        mod = $('available select').val();
-    $.ajax({
-        url: '/cart/add',
-        data: {$id: id, $qty: $qty,  $mod: mod},
-        type: 'GET',
-        success: function (res) {
-            showCart(res);
-        },
-        error: function () {
-            alert('Ошибка! Поробуйте позже');
-        }
-    });
-}
+/*Cart*/
+$('body').on('click', '.add-to-cart-link', function(e){
+     e.preventDefault();
+     var id = $(this).data('id'),
+         qty = $('.quantity input').val() ? $('.quantity input').val() : 1,
+         mod = $('.available select').val();
+     $.ajax({
+         url: '/cart/add',
+         data: {id: id, qty: qty, mod: mod},
+         type: 'GET',
+         success: function(res){
+             showCart(res);
+         },
+         error: function(){
+             alert('Ошибка! Попробуйте позже');
+         }
+     });
+});
 
-function showCart(cart)
-{
-
+function showCart(cart){
+    console.log(cart);
 }
-/** cart */
+/*Cart*/
 
 $('#currency').change(function(){
     window.location = 'currency/change?curr=' + $(this).val();
