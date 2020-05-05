e4a05a5bd6a70693bfc3f5679cc1bfa6aa6b1ce6
alzhan-ismagulov <aismagulov@gmail.com>
2020-05-06 00:27:01 +0200

    02_20. Корзина. Часть 3

--- app/controllers/CartController.php
+++ app/controllers/CartController.php
@@ -11,7 +11,8 @@ class CartController extends AppController {
         $qty = !empty($_GET['qty']) ? (int)$_GET['qty'] : null;
         $mod_id = !empty($_GET['mod']) ? (int)$_GET['mod'] : null;
         $mod = null;
-        if($id){
+        if($id)
+        {
             $product = \R::findOne('product', 'id = ?', [$id]);
             if(!$product){
                 return false;
