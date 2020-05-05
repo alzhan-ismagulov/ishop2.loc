10e07975d123c7f7decbaefaae5eefb10417e8e5
alzhan-ismagulov <aismagulov@gmail.com>
2020-05-05 16:18:45 +0200

    02_18. Корзина. Часть 1

--- app/models/Product.php
+++ app/models/Product.php
@@ -2,18 +2,15 @@
 
 namespace app\models;
 
-class Product extends AppModel
-{
-    public function setRecentlyViewed($id)
-    {
+class Product extends AppModel {
+
+    public function setRecentlyViewed($id){
         $recentlyViewed = $this->getAllRecentlyViewed();
-        if(!$recentlyViewed)
-        {
+        if(!$recentlyViewed){
             setcookie('recentlyViewed', $id, time() + 3600*24, '/');
-        } else {
+        }else{
             $recentlyViewed = explode('.', $recentlyViewed);
-            if(!in_array($id, $recentlyViewed))
-            {
+            if(!in_array($id, $recentlyViewed)){
                 $recentlyViewed[] = $id;
                 $recentlyViewed = implode('.', $recentlyViewed);
                 setcookie('recentlyViewed', $recentlyViewed, time() + 3600*24, '/');
@@ -21,10 +18,8 @@ class Product extends AppModel
         }
     }
 
-    public function getRecentlyViewed()
-    {
-        if(!empty($_COOKIE['recentlyViewed']))
-        {
+    public function getRecentlyViewed(){
+        if(!empty($_COOKIE['recentlyViewed'])){
             $recentlyViewed = $_COOKIE['recentlyViewed'];
             $recentlyViewed = explode('.', $recentlyViewed);
             return array_slice($recentlyViewed, -3);
@@ -32,12 +27,11 @@ class Product extends AppModel
         return false;
     }
 
-    public function getAllRecentlyViewed()
-    {
-        if(!empty($_COOKIE['recentlyViewed']))
-        {
+    public function getAllRecentlyViewed(){
+        if(!empty($_COOKIE['recentlyViewed'])){
             return $_COOKIE['recentlyViewed'];
         }
         return false;
     }
+
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,29 @@
+/** cart */
+$('body').on('add-to-cart-link', function(e))
+{
+    e.preventDefault();
+    var $id = $(this).data('id'),
+        $qty = $('.quantity input').val() ? $('quantity input').val() : 1,
+        mod = $('available select').val();
+    $.ajax({
+        url: '/cart/add',
+        data: {$id: id, $qty: $qty,  $mod: mod},
+        type: 'GET',
+        success: function (res) {
+            showCart(res);
+        },
+        error: function () {
+            alert('Ошибка! Поробуйте позже');
+        }
+    });
+}
+
+function showCart(cart)
+{
+
+}
+/** cart */
+
 $('#currency').change(function(){
     window.location = 'currency/change?curr=' + $(this).val();
 });
