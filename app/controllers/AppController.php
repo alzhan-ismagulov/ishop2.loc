d69dfde312eb3d00c4251c2748ffe7316859f000
alzhan-ismagulov <aismagulov@gmail.com>
2020-05-07 15:56:26 +0200

    02_38. Фильтры. Часть 3

--- app/controllers/AppController.php
+++ app/controllers/AppController.php
@@ -8,8 +8,7 @@ use ishop\App;
 use ishop\base\Controller;
 use ishop\Cache;
 
-class AppController extends Controller
-{
+class AppController extends Controller{
 
     public function __construct($route){
         parent::__construct($route);
